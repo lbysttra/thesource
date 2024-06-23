8935b2d387c29ce9e29889a57848a9b27a823893
lbysttra <mnbvcxzmopl@gmail.com>
2024-06-23 23:20:06 +0300

    Add files via upload

--- Tepthon/assistant/bot_pms.py
+++ Tepthon/assistant/bot_pms.py
@@ -97,7 +97,7 @@ async def bot_start(event):
         zz_ch = user.username
     else:
         zz_txt = "⌔ قنـاة السـورس ⌔"
-        zz_ch = "Tepthon"
+        zz_ch = "lraqithon"
     custompic = gvarstatus("BOT_START_PIC") or None
     if chat.id != Config.OWNER_ID:
         customstrmsg = gvarstatus("START_TEXT") or None
